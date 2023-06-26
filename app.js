4715fd1ef89db58f0f9d6e3300c7c6d5552eade1
Luis <luisdevv232@gmail.com>
2023-06-25 17:17:49 -0700

    Add initial form to allow players input their names, refactored some code and added more styling

--- app.js
+++ app.js
@@ -41,18 +41,19 @@ const gameBoard = ((numberOfRows, numberOfCols) => {
 
 const Player = (name, marker) => {
     const getName = () => name;
+    const setName = (newName) => name = newName;
     const getMarker = () => marker;
 
-    return { getName, getMarker };
+    return { getName, setName, getMarker };
 };
 
-const player1 = Player("Luis", "X");
-const player2 = Player("Daniel", "O");
-
-
-const game = ((player1, player2, gameBoard) => {
+const game = ((gameBoard) => {
     let turns = 0;
     let isGameOver = false;
+
+    let player1;
+    let player2;
+
     let currentPlayer; 
 
     const setFirstPlayerToPlay = () => {
@@ -61,7 +62,11 @@ const game = ((player1, player2, gameBoard) => {
         currentPlayer = players[playerIndex];
     };
 
-    setFirstPlayerToPlay();
+    const initPlayers = (player1Name, player2Name) => {
+        player1 = Player(player1Name, "X");
+        player2 = Player(player2Name, "O");
+        setFirstPlayerToPlay();
+    };
 
     const togglePlayerTurn = () => {
         if (currentPlayer === player1) {
@@ -128,12 +133,12 @@ const game = ((player1, player2, gameBoard) => {
         if (!successfulPlacement) return { success: false, msg: "This cell is already taken" };
         if (isThereAWinner()) {
             isGameOver = true;
-            return { success: true, msg: `${currentPlayer.getName()} won!!` };
+            return { success: true, winner: currentPlayer, draw: false };
         }
         turns++;
         if (turns === 9) {
             isGameOver = true;
-            return { success: true, msg: "Draw!!" };
+            return { success: true, draw: true };
         }
         togglePlayerTurn();
         return  { success: true }
@@ -146,35 +151,81 @@ const game = ((player1, player2, gameBoard) => {
         setFirstPlayerToPlay();
     };
 
-    return { playRound, getCurrentPlayer, resetGame };
-})(player1, player2, gameBoard);
+    return { initPlayers, playRound, getCurrentPlayer, resetGame };
+})(gameBoard);
 
 
 const displayController = (() => {
+
+    const gameStartForm = document.querySelector("#game-start-form");
+
+    const mainGame = document.querySelector(".main");
+
+    const displayStart = () => {
+        const currentPlayer = game.getCurrentPlayer();
+        playerTurnElement.textContent = `It's ${currentPlayer.getName()} turn!`;
+    };
+
+    gameStartForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const firstPlayerName = formData.get("player-1-name");
+        const secondPlayerName = formData.get("player-2-name");
+        game.initPlayers(firstPlayerName, secondPlayerName);
+        gameStartForm.classList.add("hidden");
+        mainGame.classList.remove("hidden");
+        displayStart();
+    });
+
     const cellButtons = document.querySelectorAll(".cell");
+    
+    const playerTurnElement = document.querySelector(".player-turn");
+    const errorMsgElement = document.querySelector(".error-message");
+    const winnerElement = document.querySelector(".winner");
     const resetGameBtn = document.querySelector(".reset-btn");
 
     const catIcon = document.querySelector(".cat");
     const dogIcon = document.querySelector(".dog");
 
+    const putCorrespondingIcon = (marker, cellBtn) => {
+        if (marker === "X") {
+            cellBtn.appendChild(catIcon.cloneNode(true));
+        } else {
+            cellBtn.appendChild(dogIcon.cloneNode(true));
+        }
+    };
+
     const fillCell = (event) => {
-        console.log(event.target);
         // event bubbling and capturing to the svg icons >:(
-        if (event.target.className !== "cell") return;
-        const cellRow = Number(event.target.getAttribute("data-row"));
-        const cellColumn = Number(event.target.getAttribute("data-column"));
+        // the svg are childs of the cell buttons, so whe a cell button is clicked,
+        // is possible that instead of the button being the event.target, its the svg icons
+        // with this check, its making sure that the button cell its going to be the event.target
+        const cellBtn = event.target.className === "cell" ? event.target : event.target.closest(".cell");
+        const cellRow = Number(cellBtn.getAttribute("data-row"));
+        const cellColumn = Number(cellBtn.getAttribute("data-column"));
         const currentPlayer = game.getCurrentPlayer();
-        console.log({ target: event.target} );
-        const { success, msg } = game.playRound(cellRow, cellColumn);
-         if (success) {
-            if (currentPlayer.getMarker() === "X") {
-                event.target.appendChild(catIcon.cloneNode(true));
-            } else {
-                event.target.appendChild(dogIcon.cloneNode(true));
-            }
-            // event.target.textContent = currentPlayer.getMarker();
-        } 
-        console.log(msg);
+        const { success, msg, winner, draw } = game.playRound(cellRow, cellColumn);
+        if (!success) {
+            errorMsgElement.textContent = msg;
+            return;
+        }
+        // clear error msg if there is any
+        errorMsgElement.textContent = "";
+        putCorrespondingIcon(currentPlayer.getMarker(), cellBtn);
+        if (!winner && !draw) {
+            // post player after the current player played
+            playerTurnElement.textContent = `It's ${game.getCurrentPlayer().getName()} turn!`;
+            return;
+        }
+
+        if (draw) {
+            winnerElement.textContent = `Draw!`;
+        } else {
+            // if there is a winner
+            winnerElement.textContent = `${winner.getName()} won!`;
+        }
+        playerTurnElement.textContent = "";
+        errorMsgElement.textContent = "";
     };
     
     cellButtons.forEach(cellButton => {
@@ -186,5 +237,8 @@ const displayController = (() => {
         cellButtons.forEach(cellButton => {
             cellButton.textContent = "";
         });
+        playerTurnElement.textContent = `It's ${game.getCurrentPlayer().getName()} turn!`;
+        errorMsgElement.textContent = "";
+        winnerElement.textContent = "----";
     });
 })();
